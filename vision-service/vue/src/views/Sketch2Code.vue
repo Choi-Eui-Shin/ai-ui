5f562dacc9468092ac946dbc6a59ac3b30f7ed0f
AI-DEV\CHOI <dreamsalmon@gmail.com>
2023-12-12 09:08:16 +0900

    부자

--- vision-service/vue/src/views/Sketch2Code.vue
+++ vision-service/vue/src/views/Sketch2Code.vue
@@ -7,8 +7,45 @@
                 </v-col>
             </v-row>
             <v-row>
-                <v-col cols="12">
+                <v-col cols="9">
                     <canvas ref="drawCanvas" style="overflow-y: auto; overflow-x: auto; padding: 0; border-style: solid; border-color: antiquewhite;"></canvas>
+                </v-col>
+                <v-col cols="3">
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field
+                                label="클래스"
+                                hide-details="auto"
+                                outlined
+                                readonly
+                                v-model="className"
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field
+                                label="프로퍼티 이름"
+                                hide-details="auto"
+                                outlined
+                                v-model="propertyName"
+                                @input="propertyChanged"
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-select
+                                v-model="selectedEvents"
+                                :items="events"
+                                chips
+                                label="이벤트"
+                                multiple
+                                outlined
+                                @input="eventChanged"
+                            ></v-select>
+                        </v-col>
+                    </v-row>
                 </v-col>
             </v-row>
         </v-container>
@@ -83,6 +120,31 @@ export default {
              * fabric canvas
              */
             drawCanvas: null,
+            /**
+             * 이미지 분석 결과
+             */
+            uiElements: [],
+            /**
+             * 이벤트 목록
+             */
+            events: ['click', 'changed'],
+            /**
+             * 클래스 이름
+             */
+            className: '',
+            /**
+             * 프로퍼티 이름
+             */
+            propertyName: '',
+            /**
+             * 선택 이벤트
+             */
+            selectedEvents: [],
+            /**
+             * 선택된 UI 요소
+             */
+            selectedUiObject: null,
+
         };
     },
 
@@ -95,7 +157,56 @@ export default {
         this.onResize();
         window.addEventListener('resize', this.onResize, { passive: true });
 
-        this.drawCanvas = new fabric.Canvas(this.$refs.drawCanvas);
+        /**
+         * fabric 캔버스
+         */
+        let canvas = new fabric.Canvas(this.$refs.drawCanvas);
+        this.drawCanvas = canvas;
+        let _ckHandler = this.clickHandler;
+
+        // 이벤트 등록
+        canvas.on('mouse:down', function(opt) {
+            var evt = opt.e;
+            if (evt.altKey === true) {
+                this.isDragging = true;
+                this.selection = false;
+                this.lastPosX = evt.clientX;
+                this.lastPosY = evt.clientY;
+            }
+            else {
+                _ckHandler(opt);
+            }
+        });
+        canvas.on('mouse:move', function(opt) {
+        if (this.isDragging) {
+            var e = opt.e;
+            var vpt = this.viewportTransform;
+            vpt[4] += e.clientX - this.lastPosX;
+            vpt[5] += e.clientY - this.lastPosY;
+            this.requestRenderAll();
+            this.lastPosX = e.clientX;
+            this.lastPosY = e.clientY;
+        }
+        });
+        canvas.on('mouse:up', function() {
+            // on mouse up we want to recalculate new interaction
+            // for all objects, so we call setViewportTransform
+            this.setViewportTransform(this.viewportTransform);
+            this.isDragging = false;
+            this.selection = true;
+        });
+        canvas.on('mouse:wheel', function(opt) {
+            let delta = opt.e.deltaY;
+            let zoom = canvas.getZoom();
+            zoom *= 0.999 ** delta;
+            if (zoom > 20) zoom = 20;
+            if (zoom < 0.01) zoom = 0.01;
+            canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+            opt.e.preventDefault();
+            opt.e.stopPropagation();
+        });
+    
+
     },
     
     computed: {
@@ -104,7 +215,8 @@ export default {
     methods: {
         onResize () {
             let can = this.$refs.drawCanvas;
-            can.width = window.innerWidth-26;
+            let w = window.innerWidth-26;
+            can.width = w * (3/4);
             can.height = window.innerHeight-120;
         },
 
@@ -149,6 +261,61 @@ export default {
         },
 
         /**
+         * 이벤트 선택 변경
+         * @param {*} item 
+         */
+        eventChanged(item) {
+            if(item == null)
+                return;
+
+            if(this.selectedUiObject !== null) {
+                this.selectedUiObject['events'] = item;
+            }
+        },
+
+        /**
+         * 프로퍼티 변경
+         * @param {*} newName 
+         */
+        propertyChanged(newName) {
+            if(this.selectedUiObject !== null) {
+                this.selectedUiObject['propertyName'] = newName;
+            }
+        },
+        
+        /**
+         * UI
+         * @param {*} options 
+         */
+        clickHandler(options) {
+            if(options.target) {
+                let pos = this.uiElements.findIndex(m => m.uid === options.target.uid);
+                if(pos !== -1) {
+                    let obj = this.uiElements[pos];
+                    this.className = obj.classId;
+                    this.propertyName = obj.propertyName;
+                    this.selectedEvents = obj.events;
+
+                    this.selectedUiObject = obj;
+                }
+                else {
+                    this.selectedUiObject = null;
+
+                    this.className = '';
+                    this.propertyName = '';
+                    this.selectedEvents = [];
+                }
+            }
+            else {
+                this.selectedUiObject = null;
+
+                this.className = '';
+                this.propertyName = '';
+                this.selectedEvents = [];
+            }
+        },
+
+        /**
          * 분석 결과를 화면에 표시
          * @param {*} result 
          */
@@ -159,6 +326,7 @@ export default {
              * 초기화
              */
             this.drawCanvas.clear();
+            this.uiElements = [];
             
             let thisCanvas = this.drawCanvas;
             
@@ -166,6 +334,10 @@ export default {
              * 추출 객체 표시
              */
             res.result.forEach(elm => {
+                elm['propertyName'] = '';
+                elm['events'] = [];
+                this.uiElements.push(elm);
+
                 // let x = elm.rect.x;
                 // let y = elm.rect.y;
                 // let width = elm.rect[2] - elm.rect[0];
@@ -180,6 +352,9 @@ export default {
                     height: elm.rect.height,
                     borderColor: 'blue'
                 });
+
+                // 클릭 이벤트에서 사용할 객체 키
+                rect.set('uid', elm.uid);
 
                 disableControlVisible(rect);
                 // thisCanvas.add(rect);
